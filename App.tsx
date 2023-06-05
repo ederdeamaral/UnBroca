2c0790bbc8a15d44bcd3cdc76079def40c9d7d43
Wagner <wagnerweb2010@gmail.com>
2023-06-05 14:50:53 -0300

    Created some android safe space formatting

--- App.tsx
+++ App.tsx
@@ -1,17 +1,37 @@
 import { StatusBar } from "expo-status-bar";
 import { StrictMode } from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import { ColorPalette } from "./assets/Globals";
-import { MainClientPage } from "./Components/Pages"
+import { MainClientPage } from "./Components/Pages";
 
 export default function App() {
   return (
     <StrictMode>
-    <View style={{backgroundColor: ColorPalette.Gray1, flex:1}}>
+    <View style={styles.app}>
+      <View style={[styles.safeSpace, styles.androidSafeSpace]}>
         <MainClientPage/>
         <StatusBar />
+      </View>
     </View>
     </StrictMode>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    backgroundColor: ColorPalette.Gray1,
+    flex:1,
+  },
+
+  safeSpace: {
+    flex: 1,
+    overflow: "hidden",
+  },
+
+  androidSafeSpace: {
+    position: "absolute",
+    top:25,
+    bottom: 0,
+  }
+});
