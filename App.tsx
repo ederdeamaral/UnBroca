25cf022d5171ed824be1d4537a0d9ec0230f9bfe
Wagner <wagnerweb2010@gmail.com>
2023-06-07 01:40:26 -0300

    added a orange bar to the top

--- App.tsx
+++ App.tsx
@@ -20,18 +20,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   app: {
-    backgroundColor: ColorPalette.Gray1,
+    backgroundColor: ColorPalette.Primary,
     flex:1,
   },
 
   safeSpace: {
+    backgroundColor: ColorPalette.Gray1,
     flex: 1,
     overflow: "hidden",
   },
 
   androidSafeSpace: {
     position: "absolute",
-    top:25,
+    left: 0,
+    right: 0,
+    top:40,
     bottom: 0,
   }
 });
