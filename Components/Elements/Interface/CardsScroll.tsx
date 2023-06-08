95579163753c853e89844f9672e60decbe399366
Wagner <wagnerweb2010@gmail.com>
2023-06-08 00:44:23 -0300

    Adjusted CardScroll style

--- Components/Elements/Interface/CardsScroll.tsx
+++ Components/Elements/Interface/CardsScroll.tsx
@@ -1,50 +1,51 @@
-import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, Text, StyleSheet, View } from "react-native";
 import { Card, CardProps } from "./Card";
+import { ColorPalette } from "../../../assets/Globals";
 
-interface Props { 
-  cards? : CardProps[]
+interface Props {
+  cards?: CardProps[];
+}
+
+const renderer = (item: CardProps, index: number) => {
+  return (
+      <Card
+        name     = {item.name}
+        distance = {item.distance}
+        price    = {item.price}
+        imageSrc = {item.imageSrc}
+        style    = {{ flex:1/2, margin: 5}}
+      />
+  );
 };
 
-export function CardsScroll({cards}: Props) {
+export function CardsScroll({ cards }: Props) {
   return (
     <FlatList
       data={cards}
       style={styles.productView}
       numColumns={2}
-      ItemSeparatorComponent={() => <View style={{ width: 10, height:10 }} />}
       renderItem={({ item, index }) => {
-        return (
-          <View
-            style={[
-              {
-                flexGrow: 1,
-                width: "50%",
-                position: "relative",
-              },
-              index % 2 === 0
-                ? {
-                    paddingRight: 5,
-                  }
-                : {
-                    paddingLeft: 5,
-                  },
-            ]}
-          >
-            <Card
-              name={item.name}
-              distance={item.distance}
-              price={item.price}
-              imageSrc={item.imageSrc}
-            />
-          </View>
-        );
+        return renderer(item, index);
       }}
+      ListEmptyComponent={
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 18,
+            textAlign: "center",
+            color: ColorPalette.Gray2,
+          }}
+        >
+          Sem items para mostrar!
+        </Text>
+      }
     />
   );
-};
+}
 
 const styles = StyleSheet.create({
   productView: {
     flex: 1,
+    alignContent: "space-around",
   },
 });
